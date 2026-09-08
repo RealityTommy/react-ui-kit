b09b4e56cb65e1b0b6f74651a58abd24ed670952
Tommy Truong <tommy@truong.rocks>
2026-09-08 09:50:13 -0400

    add Prettier, format-on-save config, and lint/format scripts

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
+import prettier from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -16,6 +17,7 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
       jsxA11y.flatConfigs.recommended,
+      prettier,
     ],
     languageOptions: {
       globals: globals.browser,
